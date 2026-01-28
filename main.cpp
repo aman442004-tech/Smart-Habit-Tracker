cbdb1919cbcec057a582eeeb3b2b7790c9c46fe6
Ujjwal2005-12 <ujjwalsharma928@gmail.com>
2026-01-28 10:35:10 +0530

    code structure

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class habit{
+
+    
+    
+    
+    friend void display();
+};
+
+void markcomplete();
+
+inline int streak();
+
+int main(){
+
+
+    return 0;
+}
